e9b8576877f5345bb7ee65a90a0286e0a19604e2
dustin-py <dustinstri92@gmail.com>
2023-10-31 07:49:43 -0700

    commit

--- Glass_App/polls/models.py
+++ Glass_App/polls/models.py
@@ -1,3 +1,9 @@
+
+# Change your models (in models.py).
+# Run python manage.py makemigrations to create migrations for those changes
+# Run python manage.py migrate to apply those changes to the database.
+
+
 from django.db import models
 
 # Create your models here.
